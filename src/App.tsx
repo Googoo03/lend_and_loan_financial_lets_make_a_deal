00170b61fbc3f0b38d6e3e42aca135d37ad3b654
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-11-01 15:10:32 -0700

    Title logo test:

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Title from "./components/Title.tsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,10 @@
+import Logo from "./assets/LetsMakeADealLogo.jpg";
+
 function Title() {
   return (
     <>
       <div>
+        <img src={Logo} alt="Lets Make a Deal Logo" width={"100%"} />
         <h1>Lets Make a Deal Sample Title</h1>
       </div>
     </>
