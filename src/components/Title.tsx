338f80b412529e65eddc1430df36ec91dc1e1993
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-11-01 15:46:03 -0700

    routing changes

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import Logo from "../assets/LetsMakeADealLogo.jpg";
 
 function Title() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/door1");
+  };
+
   return (
     <>
       <div>
@@ -9,6 +15,7 @@ function Title() {
           src={Logo}
           alt="Lets Make a Deal Logo"
           width={"100%"}
+          onClick={handleClick}
         />
         <h1>Lets Make a Deal Sample Title</h1>
       </div>
