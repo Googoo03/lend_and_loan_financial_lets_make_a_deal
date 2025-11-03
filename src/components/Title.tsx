6721cc95d3a228ef9914fdb4105aa88f39c63525
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-11-03 10:54:24 -0800

    Added title animations

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/LetsMakeADealLogo.png";
 import Host from "../assets/Lets-Make-A-Deal-Host.png";
+import { motion } from "framer-motion";
 
 interface TitleProps {
   clickLink: string;
@@ -15,14 +16,23 @@ function Title({ clickLink }: TitleProps) {
   return (
     <>
       <div className="center-horizontal">
-        <img
+        <motion.img
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
           className={"logo"}
           src={Logo}
           alt="Lets Make a Deal Logo"
           width={"100%"}
           onClick={handleClick}
         />
-        <img className={"host"} src={Host} alt="Hunk" />
+        <motion.img
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          className={"host"}
+          src={Host}
+          alt="Hunk"
+        />
       </div>
     </>
   );
