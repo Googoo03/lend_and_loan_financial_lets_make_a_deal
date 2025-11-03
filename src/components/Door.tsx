11e7945259562d03da8adcb4ed192967f178b966
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-11-03 11:04:55 -0800

    Door animations

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -1,5 +1,6 @@
 import Curtain from "../assets/Curtain.png";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 interface DoorProps {
   number: number;
@@ -8,19 +9,24 @@ interface DoorProps {
 }
 
 function Door({ number, action }: DoorProps) {
+  const [visible, setVisible] = useState(false);
   return (
     <>
-      <motion.img
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        border-radius={"25px"}
-        height={"100%"}
-        src={Curtain}
-        alt={"Door" + number}
-        onClick={() => {
-          action && action();
-        }}
-      />
+      {visible && (
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ y: 100, opacity: 0 }}
+          whileHover={{ y: 50 }}
+          border-radius={"25px"}
+          height={"100%"}
+          src={Curtain}
+          alt={"Door" + number}
+          onClick={() => {
+            setVisible(false);
+          }}
+        />
+      )}
     </>
   );
 }
