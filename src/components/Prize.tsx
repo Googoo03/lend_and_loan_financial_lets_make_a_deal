b479a9fb49a877bef256336bddd2be58f74be7f1
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-11-05 22:53:27 -0800

    simple animation for prizes

--- src/components/Prize.tsx
+++ src/components/Prize.tsx
@@ -1,3 +1,5 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 interface PrizeProps {
   url: string;
 }
@@ -6,8 +8,14 @@ export default function Prize({ url }: PrizeProps) {
   if (!url) return <p>Loading...</p>;
 
   return (
-    <div className="rounded-3 overflow-hidden d-inline-block">
-      <img className="img-fluid" src={url} alt="Random prize" />
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        className="rounded-3 overflow-hidden d-inline-block"
+      >
+        <img className="img-fluid" src={url} alt="Random prize" />
+      </motion.div>
+    </AnimatePresence>
   );
 }
