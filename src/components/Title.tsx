ed820a2282820d02588354de0ab8b870b3e2965d
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-11-05 23:21:27 -0800

    Host click animation

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -26,13 +26,35 @@ function Title({ clickLink }: TitleProps) {
           width={"100%"}
           onClick={handleClick}
         />
-        <motion.img
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          className={"host"}
-          src={Host}
-          alt="Hunk"
-        />
+        <div
+          style={{
+            position: "fixed", // or "absolute" if inside a relative container
+            left: 0,
+            bottom: 0,
+            width: "500px", // or whatever size you want
+            height: "500px",
+            overflow: "hidden", // ✅ hides any part of the image that overflows
+
+            zIndex: 3,
+          }}
+        >
+          <motion.img
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            whileTap={{ scaleX: 0.9, scaleY: 1.1 }}
+            style={{
+              position: "absolute", // position relative to parent
+              bottom: 0, // stick to bottom
+              left: 0, // stick to left
+              width: "500px", // control image size
+              height: "auto",
+              objectFit: "contain",
+              zIndex: 4,
+            }}
+            src={Host}
+            alt="Hunk"
+          />
+        </div>
       </div>
     </>
   );
