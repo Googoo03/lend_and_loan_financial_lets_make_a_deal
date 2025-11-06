9058068bd52e197b04e7e4ace03aa094d8549b9b
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-11-05 21:48:49 -0800

    Added tokens for reading

--- api/prizes/list.mjs
+++ api/prizes/list.mjs
@@ -2,7 +2,7 @@ import { list } from "@vercel/blob";
 import { NextResponse } from "next/server.js";
 
 export async function GET() {
-  const { blobs } = await list({ prefix: "prizes/" });
+  const { blobs } = await list({ prefix: "prizes/", token: process.env.LNLLMAD_READ_WRITE_TOKEN });
 
   return NextResponse.json({ prizes: blobs });
 }
